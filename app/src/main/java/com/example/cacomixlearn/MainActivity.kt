32e115d4a218d1f311410ef439fc4a2a2f52b172
eduislas84 <122810400+eduislas84@users.noreply.github.com>
2024-12-03 18:26:59 +0000

    03-12-24

---
+++ app/src/main/java/com/example/cacomixlearn/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.cacomixlearn
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.cacomixlearn.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        enableEdgeToEdge()
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Configurar el evento de inicio de sesión
+        binding.btnLogin.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            if (validateInput(email, password)) {
+                // Redirigir a la nueva actividad
+                val intent = Intent(this, PruebaActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun validateInput(email: String, password: String): Boolean {
+        return email.isNotEmpty() && password.isNotEmpty()
+    }
+}
---
+++ app/src/main/java/com/example/cacomixlearn/PruebaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cacomixlearn
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PruebaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.prueba) // Asegúrate de que prueba.xml existe en res/layout
+    }
+}
